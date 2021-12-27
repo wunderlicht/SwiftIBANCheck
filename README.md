a452585e0f16ab7a63872294ae133c46f18787cf
wunderlicht <wunderlich@actualchange.eu>
2021-12-28 00:26:49 +0100

    Chore get rid of dependency and convert to package

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # IBANCheck
 
-A description of this package.
+IBANCheck provides functionality to check IBANs for correctness.
+
+See https://en.wikipedia.org/wiki/International_Bank_Account_Number#Algorithms for details on how it works.
+
+It all started when back in 2019 a team said they can't do it on time, so I volunteered. I took it as a side job to help out the company I was working for and also to et into Swift.
--- Tests/IBANCheckTests/IBANCheckTests.swift
+++ Tests/IBANCheckTests/IBANCheckTests.swift
@@ -1,11 +1,188 @@
+//
+//  IBANCheckTests.swift
+//  IBAN CheckTests
+//
+//  Created by Torsten Wunderlich on 31.01.17.
+//
 import XCTest
 @testable import IBANCheck
 
-final class IBANCheckTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(IBANCheck().text, "Hello, World!")
+
+class IBANChecksTests: XCTestCase {
+// letter2Num tests
+    func testLetter2NumNormalOperation() {
+        let parameter = "D"
+        let expected = "13" //A=10, B=11, C=12, D=13
+        XCTAssertEqual(IBANChecks.letter2Num(parameter),expected)
+    }
+    
+    func testLetter2NumReturnsNilIfLetterNotFound() {
+        let parameter = "Ä"
+        XCTAssertNil(IBANChecks.letter2Num(parameter))
+    }
+
+    func testLetter2NumReturnsNilIfStringNotFound() {
+        let parameter = "hello"
+        XCTAssertNil(IBANChecks.letter2Num(parameter))
+    }
+    
+// string2Num tests
+    func testString2NumStringNormalOperation() {
+        let parameter = "DE"
+        let expected = "1314" //A=10, B=11, C=12, D=13
+        XCTAssertEqual(IBANChecks.string2NumString(letters: parameter),expected)
+    }
+
+    func testString2NumStringGB() {
+        let parameter = "GB"
+        let expected = "1611" //A=10, B=11, C=12, D=13
+        XCTAssertEqual(IBANChecks.string2NumString(letters: parameter),expected)
+    }
+    
+    func testString2NumStringWithNumbers() {
+        let parameter = "DE0129"
+        let expected  = "13140129"
+        XCTAssertEqual(IBANChecks.string2NumString(letters: parameter), expected)
+    }
+    
+    func testString2NumStringReturnsNilIfLetterNotFound() {
+        let parameter = "DÄ"
+        XCTAssertNil(IBANChecks.string2NumString(letters: parameter))
+    }
+    
+// splitIban tests
+    func testSplitIban(){
+        let iban = "DE123456789"
+        let expectedCountryCode = "DE"
+        let expectedCheckSum = "12"
+        let expectedBban = "3456789"
+        let (countryCode,checkSum,Bban) = IBANChecks.splitIban(iban: iban)
+        XCTAssertEqual(countryCode, expectedCountryCode)
+        XCTAssertEqual(checkSum, expectedCheckSum)
+        XCTAssertEqual(Bban, expectedBban)
+    }
+    
+    func testSplitIbanWhitespaces(){
+        let iban = " DE123 4567 89 "
+        let expectedCountryCode = "DE"
+        let expectedCheckSum = "12"
+        let expectedBban = "3456789"
+        let (countryCode,checkSum,Bban) = IBANChecks.splitIban(iban: iban)
+        XCTAssertEqual(countryCode, expectedCountryCode)
+        XCTAssertEqual(checkSum, expectedCheckSum)
+        XCTAssertEqual(Bban, expectedBban)
+    }
+    
+    func testSplitIbanShortString() {
+        let parameter = "12"
+        let expectedCountryCode = ""
+        let expectedCheckSum = ""
+        let expectedBban = ""
+        let (countryCode,checkSum,Bban) = IBANChecks.splitIban(iban: parameter)
+        XCTAssertEqual(countryCode, expectedCountryCode)
+        XCTAssertEqual(checkSum, expectedCheckSum)
+        XCTAssertEqual(Bban, expectedBban)
+        
+    }
+    
+// checkIban tests
+    func testCheckIbanGermany(){
+        let iban = "DE89370400440532013000"
+        XCTAssertTrue(IBANChecks.check(iban: iban))
+    }
+
+    func testCheckIbanWithSpacesGermany(){
+        let iban = "DE89 3704 0044 0532 0130 00"
+        XCTAssertTrue(IBANChecks.check(iban: iban))
+    }
+
+    func testCheckIbanWithSpacesUK(){
+        let iban = "GB29 NWBK 6016 1331 9268 19"
+        XCTAssertTrue(IBANChecks.check(iban: iban))
+    }
+
+
+    func testCheckIbanWithABunchOfIbans() {
+        //taken from http://www.rbs.co.uk/corporate/international/g0/guide-to-international-business/regulatory-information/iban/iban-example.ashx
+        let ibans = [
+            "Just long enough": "AB451",
+            "Albania": "AL47 2121 1009 0000 0002 3569 8741",
+            "Austria": "AT61 1904 3002 3457 3201",
+            "France" : "FR14 2004 1010 0505 0001 3M02 606",
+            "Germany": "DE89 3704 0044 0532 0130 00",
+            //"United Kingdom": "GB29 RBOS 6016 1331 9268 19" This is actually a false number
+            "United Kingdom": "GB29 NWBK 6016 1331 9268 19",
+            "Italy" : "IT40 S054 2811 1010 0000 0123 456",
+            "Liechtenstein" : "LI21 0881 0000 2324 013A A",
+            "Spain" : "ES80 2310 0001 1800 0001 2345",
+            "Switzerland" : "CH93 0076 2011 6238 5295 7"
+        ]
+        for (country, iban) in ibans {
+            XCTAssertTrue(IBANChecks.check(iban: iban), "Did't work for " + country)
+        }
+    }
+    
+    func testCheckIbanWithABunchOfFaultyIbans() {
+        //taken from http://www.rbs.co.uk/corporate/international/g0/guide-to-international-business/regulatory-information/iban/iban-example.ashx
+        let ibans = [
+            "Too Short": "AB12",
+            "Long Enough but apha checksum": "ABA51",
+            "Albania": "AL47 2121 1009 0000 0002 3560 8741",
+            "Austria": "AT61 1904 3002 3757 3201",
+            "France" : "FR14 2004 1010 A505 0001 3M02 606",
+            "Germany": "DE89 3704 0Ä44 0532 0130 00",
+            "United Kingdom": "GB29 RBOS 6016 1331 9268 19", //This is actually a false number
+            "United Kingdom2": "GB29 NWKK 6016 1331 9268 19",
+            "Italy" : "IT40 S054 281A 1010 0000 0123 456",
+            "Liechtenstein" : "LI21 0881 0000 2324 0131 A",
+            "Spain" : "ES81 2310 0001 1800 0001 2345",
+            "Switzerland" : "CH93 0076 2011 6238 529B 7"
+        ]
+        for (country, iban) in ibans {
+            XCTAssertFalse(IBANChecks.check(iban: iban), "Did't work for " + country)
+        }
+    }
+    
+// mod tests
+    func testModBiNumZero() {
+        let bignum = "0"
+        let divident = 4
+        let expected = 0
+        XCTAssertEqual(IBANChecks.mod(bignum: bignum, div: divident), expected)
+    }
+    
+    func testModBiNumZeros() {
+        let bignum = "000000000"
+        let divident = 4
+        let expected = 0
+        XCTAssertEqual(IBANChecks.mod(bignum: bignum, div: divident), expected)
+    }
+    
+    func testModNormalOperation() {
+        let bignum = "123456789012345678901234567890123"
+        let divident = 10
+        let expected = 3
+        XCTAssertEqual(IBANChecks.mod(bignum: bignum, div: divident), expected)
+    }
+
+    func testModNodigit() {
+        let bignum = "1234567a"
+        let divident = 10
+        let expected = -1
+        XCTAssertEqual(IBANChecks.mod(bignum: bignum, div: divident), expected)
+    }
+
+    func testModDividentZero() {
+        let bignum = "1234567"
+        let divident = 0
+        let expected = -1
+        XCTAssertEqual(IBANChecks.mod(bignum: bignum, div: divident), expected)
+    }
+    
+    func testModDividenNegative() {
+        let bignum = "1234567"
+        let divident = -10
+        let expected = -1
+        XCTAssertEqual(IBANChecks.mod(bignum: bignum, div: divident), expected)
     }
 }
